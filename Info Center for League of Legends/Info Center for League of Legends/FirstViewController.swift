95516ba5fce9c768db0f117614005d3100d174a9
Victor Ilisei <vilisei@gmail.com>
2016-06-14 09:50:24 -0700

    Initial template

---
+++ Info Center for League of Legends/Info Center for League of Legends/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/14/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
