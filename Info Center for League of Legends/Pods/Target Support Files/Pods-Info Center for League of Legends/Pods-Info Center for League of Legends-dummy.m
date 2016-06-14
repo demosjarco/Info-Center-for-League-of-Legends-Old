47421ff44821dcac073e109ce3dcb23aabf0b07e
Victor Ilisei <vilisei@gmail.com>
2016-06-14 16:33:39 -0700

    Added pods

---
+++ Info Center for League of Legends/Pods/Target Support Files/Pods-Info Center for League of Legends/Pods-Info Center for League of Legends-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Info_Center_for_League_of_Legends : NSObject
+@end
+@implementation PodsDummy_Pods_Info_Center_for_League_of_Legends
+@end
