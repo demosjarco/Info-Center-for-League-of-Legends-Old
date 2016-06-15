9fc9197bb55c1fd85bb4ac215079bda292ddcc1b
Victor Ilisei <vilisei@gmail.com>
2016-06-14 20:22:01 -0700

    Added firebase config

--- Info Center for League of Legends/Info Center for League of Legends/AppDelegate.swift
+++ Info Center for League of Legends/Info Center for League of Legends/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
+        
+        /*FIRAuth.auth()?.signInAnonymously(completion: { (user, error) in
+            NSLog("User id: %@", user!.uid)
+        })*/
+        FIRAuth.auth()?.signIn(withEmail: "vilisei@gmail.com", password: "victor", completion: { (user, error) in
+            print("User id: " + user!.uid)
+        })
         
         return true
     }
