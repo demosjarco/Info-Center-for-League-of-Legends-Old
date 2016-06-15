28eca434ccd485030bc33aaf7b2ac340f57f6a13
Victor Ilisei <vilisei@gmail.com>
2016-06-15 08:56:04 -0700

    Added FMDB bridging header

---
+++ Info Center for League of Legends/Info Center for League of Legends/Objective-C/FMDB-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  FMDB-Bridging-Header.h
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/15/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+#ifndef FMDB_Bridging_Header_h
+#define FMDB_Bridging_Header_h
+
+
+#endif /* FMDB_Bridging_Header_h */
+
+#import "FMDB.h"
