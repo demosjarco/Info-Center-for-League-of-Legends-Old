c98831f7dca683e48ab2b85ea4c5bbb9f05c77a3
Victor Ilisei <vilisei@gmail.com>
2016-06-14 20:50:50 -0700

    Added edit button

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileSearch.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileSearch.swift
@@ -14,11 +14,7 @@ class ProfileSearch: MainTableViewController {
         super.viewDidLoad()
         ExistingAppChecker().checkIfAppSetup(viewController: self)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     // MARK: - Table view data source
