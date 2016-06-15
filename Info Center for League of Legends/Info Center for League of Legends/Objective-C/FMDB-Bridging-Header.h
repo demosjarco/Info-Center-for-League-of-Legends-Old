e3256b7d5d2215f9132e908195a8e5ffc1f4eadc
Victor Ilisei <vilisei@gmail.com>
2016-06-15 10:38:46 -0700

    Removed unnecessary if

--- Info Center for League of Legends/Info Center for League of Legends/Objective-C/FMDB-Bridging-Header.h
+++ Info Center for League of Legends/Info Center for League of Legends/Objective-C/FMDB-Bridging-Header.h
@@ -6,10 +6,4 @@
 //  Copyright © 2016 Tech Genius. All rights reserved.
 //
 
-#ifndef FMDB_Bridging_Header_h
-#define FMDB_Bridging_Header_h
-
-
-#endif /* FMDB_Bridging_Header_h */
-
 #import <FMDB/FMDB.h>
