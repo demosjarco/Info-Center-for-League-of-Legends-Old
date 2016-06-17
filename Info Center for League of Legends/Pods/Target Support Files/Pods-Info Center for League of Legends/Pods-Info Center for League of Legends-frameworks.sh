713357b5d6964932f798306796604fc2d71987af
Victor Ilisei <vilisei@gmail.com>
2016-06-16 20:12:34 -0700

    Added AMPopTip

--- Info Center for League of Legends/Pods/Target Support Files/Pods-Info Center for League of Legends/Pods-Info Center for League of Legends-frameworks.sh
+++ Info Center for League of Legends/Pods/Target Support Files/Pods-Info Center for League of Legends/Pods-Info Center for League of Legends-frameworks.sh
@@ -85,6 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AMPopTip/AMPopTip.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSBadgeView/JSBadgeView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBCircularProgressBar/MBCircularProgressBar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SGSStaggeredFlowLayout/SGSStaggeredFlowLayout.framework"
@@ -92,6 +93,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AMPopTip/AMPopTip.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSBadgeView/JSBadgeView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBCircularProgressBar/MBCircularProgressBar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SGSStaggeredFlowLayout/SGSStaggeredFlowLayout.framework"
