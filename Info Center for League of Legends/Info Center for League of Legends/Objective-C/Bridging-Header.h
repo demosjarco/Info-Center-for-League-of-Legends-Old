17886147e65735b9ead30168e973179741a5d420
Victor Ilisei <vilisei@gmail.com>
2016-06-17 09:38:44 -0700

    Created bridging header for sqlite

---
+++ Info Center for League of Legends/Info Center for League of Legends/Objective-C/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/16/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+
+#import <sqlite3.h>
