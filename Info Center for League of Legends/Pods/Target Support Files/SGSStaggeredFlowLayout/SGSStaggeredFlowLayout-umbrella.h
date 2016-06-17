dbf3e5c37aad3db8ef80ea2b5126d5784affb503
Victor Ilisei <vilisei@gmail.com>
2016-06-16 20:07:06 -0700

    Added SGSStaggeredFlowLayout

---
+++ Info Center for League of Legends/Pods/Target Support Files/SGSStaggeredFlowLayout/SGSStaggeredFlowLayout-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "SGSStaggeredFlowLayout.h"
+
+FOUNDATION_EXPORT double SGSStaggeredFlowLayoutVersionNumber;
+FOUNDATION_EXPORT const unsigned char SGSStaggeredFlowLayoutVersionString[];
+
