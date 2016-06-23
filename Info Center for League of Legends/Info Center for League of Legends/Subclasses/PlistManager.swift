554eb1e03cc6f930ddb968c639f4332c796fc0c4
Victor Ilisei <vilisei@gmail.com>
2016-06-23 08:26:05 -0700

    Created plist manager to help with reading of plist files

--- Info Center for League of Legends/Info Center for League of Legends/Subclasses/PlistManager.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Subclasses/PlistManager.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+class PlistManager: NSObject {
+    let baseDatabaseDirectory: String = "/databases"
+    
+    let recentSummonersFileName: String = "/recentSummoners.plist"
+    let rankedSummonerHistoryDirectoy: String = "/rankedSummonerHistory"
+    
+    func getDocumentDirectory() -> String {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+    }
+    
+    func loadRecentSummoners() -> NSArray {
+        if let recentSummoners = NSArray(contentsOfFile: getDocumentDirectory().appending(baseDatabaseDirectory).appending(recentSummonersFileName)) {
+            return recentSummoners
+        } else {
+            return NSArray()
+        }
+    }
+    func addToRecentSummoners(newSummoner: NSDictionary) -> NSArray {
+        let recentSummoners = NSMutableArray(array: loadRecentSummoners())
+        
+        recentSummoners.add(newSummoner)
+        
+        recentSummoners.write(toFile: getDocumentDirectory().appending(baseDatabaseDirectory).appending(recentSummonersFileName), atomically: true)
+        return NSArray(array: recentSummoners)
+    }
+    func moveItemInRecentSummoners(oldIndex: Int, newIndex: Int) -> NSArray {
+        let recentSummoners = NSMutableArray(array: loadRecentSummoners())
+        
+        let item = recentSummoners.object(at: oldIndex)
+        recentSummoners.removeObject(at: oldIndex)
+        recentSummoners.insert(item, at: newIndex)
+        
+        recentSummoners.write(toFile: getDocumentDirectory().appending(baseDatabaseDirectory).appending(recentSummonersFileName), atomically: true)
+        return NSArray(array: recentSummoners)
+    }
+}
