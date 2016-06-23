93b90adc0d6ea6f9ce54d1c34c423e84d030ba4c
Victor Ilisei <vilisei@gmail.com>
2016-06-23 07:22:46 -0700

    Dropped SQLite in favor of plist files

---
+++ Info Center for League of Legends/Info Center for League of Legends/Subclasses/PlistManager.swift
@@ -0,0 +1,9 @@
+//
+//  PlistManager.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/23/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import Foundation
