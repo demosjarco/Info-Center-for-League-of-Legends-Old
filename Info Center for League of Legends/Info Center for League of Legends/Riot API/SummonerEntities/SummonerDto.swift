66227b9df2a8ad10afd6babf1f00b793f8f0fd88
Victor Ilisei <vilisei@gmail.com>
2016-06-23 09:47:29 -0700

    Created summoner entity

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/SummonerEntities/SummonerDto.swift
@@ -0,0 +1,17 @@
+//
+//  SummonerDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/23/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import Foundation
+
+class SummonerDto: NSObject {
+    var summonerId: CLong = 0
+    var name: String = ""
+    var profileIconId: Int = 0
+    var revisionDate: CLong = 0
+    var summonerLevel: CLong = 0
+}
