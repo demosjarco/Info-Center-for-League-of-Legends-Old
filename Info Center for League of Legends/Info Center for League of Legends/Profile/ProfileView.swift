a7b4cb1b87aaec98a44fa45ffe60854198064833
Victor Ilisei <vilisei@gmail.com>
2016-06-25 10:57:09 -0700

    Added collection view methods

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView.swift
@@ -6,6 +6,50 @@
 //  Copyright © 2016 Tech Genius. All rights reserved.
 //
 
+import UIKit
+
 class ProfileView: MainCollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
+    // MARK: - Collection view data source
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.row {
+            case 0:
+                // Champion Mastery
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "profile_view_champ_mastery", for: indexPath) as! ProfileView_ChampMastery
+                return cell
+            default:
+                return collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+            case UICollectionElementKindSectionHeader:
+                let profileHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "profile_view_header", for: indexPath)
+                return profileHeader
+            case UICollectionElementKindSectionFooter:
+                return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "", for: indexPath)
+            default:
+                return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "", for: indexPath)
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        //
+    }
 }
