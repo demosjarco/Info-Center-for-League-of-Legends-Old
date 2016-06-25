3284fd61d32071ad33369a24289d02668fe713a0
Victor Ilisei <vilisei@gmail.com>
2016-06-25 13:56:28 -0700

    Created DDragon class ford dragon images

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/DDragon.swift
@@ -0,0 +1,13 @@
+//
+//  DDragon.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/25/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class DDragon: NSObject {
+
+}
