18a54f31ab120524953e5ae85fd68e5830209e46
Victor Ilisei <vilisei@gmail.com>
2016-06-25 08:38:17 -0700

    Created subclass for UICollectionViewControllers

---
+++ Info Center for League of Legends/Info Center for League of Legends/Subclasses/MainCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainCollectionViewController.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 6/25/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class MainCollectionViewController: UICollectionViewController {
+    
+}
