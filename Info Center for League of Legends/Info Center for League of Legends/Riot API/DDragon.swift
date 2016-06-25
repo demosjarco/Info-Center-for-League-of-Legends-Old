2a27cbccfd9d017c16d991bf123bbf61aa26151d
Victor Ilisei <vilisei@gmail.com>
2016-06-25 14:17:35 -0700

    Added getting cdn url from server alone with version for type Also added function to get url for profile icon

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/DDragon.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/DDragon.swift
@@ -6,8 +6,32 @@
 //  Copyright © 2016 Tech Genius. All rights reserved.
 //
 
-import UIKit
+import AFNetworking
 
 class DDragon: NSObject {
-
+    func getCDNurl(completion: (cdnUrl: String) -> Void) {
+        AFHTTPSessionManager().get("http://ddragon.leagueoflegends.com/realms/" + Endpoints().getRegion() + ".json", parameters: nil, progress: nil, success: { (task, responseObject) in
+            let dict = responseObject as! NSDictionary
+            completion(cdnUrl: dict["cdn"] as! String)
+        }, failure: nil)
+    }
+    
+    func getLatestDDragonVersion(dataType: String, completion: (version: String) -> Void) {
+        AFHTTPSessionManager().get("http://ddragon.leagueoflegends.com/realms/" + Endpoints().getRegion() + ".json", parameters: nil, progress: nil, success: { (task, responseObject) in
+            let dict = responseObject as! NSDictionary
+            completion(version: dict["n"]![dataType] as! String)
+        }, failure: nil)
+    }
+    
+    func getProfileIcon(profileIconId: Int, completion: (profileIconURL: URL) -> Void) {
+        self.getCDNurl { (cdnUrl) in
+            self.getLatestDDragonVersion(dataType: "profileicon", completion: { (version) in
+                do {
+                    try completion(profileIconURL: (URL(string: cdnUrl)?.appendingPathComponent(version).appendingPathComponent("img").appendingPathComponent("profileicon").appendingPathComponent(String(profileIconId)).appendingPathExtension("png"))!)
+                } catch {
+                    
+                }
+            })
+        }
+    }
 }
