668468b8df343e54fd919b0fe5508b2879aa8650
Victor Ilisei <vilisei@gmail.com>
2016-06-25 21:33:55 -0700

    Added function for first load

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView_Header.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView_Header.swift
@@ -10,6 +10,8 @@ import UIKit
 import Foundation
 
 class ProfileView_Header: UICollectionReusableView {
+    var summoner = SummonerDto()
+    
     @IBOutlet var cover:UIImageView?
     
     @IBOutlet var promotionGames:UILabel?
@@ -19,4 +21,22 @@ class ProfileView_Header: UICollectionReusableView {
     
     @IBOutlet var profilePic:UIImageView?
     @IBOutlet var statsScroller:UICollectionView?
+    
+    func initialLoad(loadedSummoner: SummonerDto) {
+        self.summoner = loadedSummoner
+        
+        DDragon().getProfileIcon(profileIconId: self.summoner.profileIconId, completion: { (profileIconURL) in
+            self.cover?.setImageWith(URLRequest(url: profileIconURL), placeholderImage: UIImage(named: "poroIcon"), success: { (request, response, image) in
+                self.cover?.image = image
+                self.profilePic?.image = image
+                
+                self.profilePic?.layer.cornerRadius = (self.profilePic?.frame.size.height)! / 2
+                }, failure: { (request, response, error) in
+                    self.cover?.image = UIImage(named: "poroIcon")
+                    self.profilePic?.image = UIImage(named: "poroIcon")
+                    
+                    self.profilePic?.layer.cornerRadius = (self.profilePic?.frame.size.height)! / 2
+            })
+        })
+    }
 }
