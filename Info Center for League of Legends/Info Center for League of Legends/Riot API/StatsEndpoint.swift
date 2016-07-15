2b82ab752f66967fbb3914892661840476c1dd95
Victor Ilisei <vilisei@gmail.com>
2016-07-14 20:03:44 -0700

    Created stats endpoint

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StatsEndpoint.swift
@@ -0,0 +1,13 @@
+//
+//  StatsEndpoint.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 7/14/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class StatsEndpoint: NSObject {
+
+}
