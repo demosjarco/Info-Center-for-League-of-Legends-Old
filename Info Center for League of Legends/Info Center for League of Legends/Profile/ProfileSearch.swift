908a09214f9891f522609dfd5b7000ae5f382675
Victor Ilisei <vilisei@gmail.com>
2016-07-21 13:51:52 -0700

    Fixed the delete summoner code from recent

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileSearch.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileSearch.swift
@@ -283,6 +283,8 @@ class ProfileSearch: MainTableViewController, UISearchBarDelegate {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            recentSummoners.removeObject(at: indexPath.row)
+            PlistManager().removeItemInRecentSummoners(oldIndex: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
