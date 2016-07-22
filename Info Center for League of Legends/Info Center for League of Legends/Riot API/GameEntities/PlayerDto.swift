437e4b41039ffd1e9b45300e8f7f2e928534837a
Victor Ilisei <vilisei@gmail.com>
2016-07-22 10:09:15 -0700

    Created entities for game endpoint

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/GameEntities/PlayerDto.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 7/22/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDto: NSObject {
+
+}
---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/GameEntities/RecentGamesDto.swift
@@ -0,0 +1,14 @@
+//
+//  RecentGamesDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 7/22/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class RecentGamesDto: NSObject {
+    var games = [GameDto]()
+    var summonerId:CLong = 0
+}
