21805a2c9916c4bae9d95037376c3a39f2b69a64
Victor Ilisei <vilisei@gmail.com>
2016-07-21 19:21:56 -0700

    Added shadow and background to summary tiles

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView_SummaryTile.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView_SummaryTile.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ProfileView_SummaryTile: UICollectionViewCell {
     func setupCell() {
-        //
+        // Shadow
+        self.layer.shadowColor = UIColor.black().cgColor
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
 }
