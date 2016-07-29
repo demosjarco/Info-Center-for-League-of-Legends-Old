ba458e2d2f4c8e406327a2c15d18414e7dd5ed44
Victor Ilisei <vilisei@gmail.com>
2016-07-29 11:19:10 -0700

    Created entities for masteries

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/SummonerEntities/MasteryPageDto.swift
@@ -0,0 +1,13 @@
+//
+//  MasteryPageDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 7/29/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class MasteryPageDto: NSObject {
+
+}
