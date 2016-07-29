bf8952614ca03ebd389013e2a731a63805638f65
Victor Ilisei <vilisei@gmail.com>
2016-07-29 11:20:52 -0700

    Created mastery endpoint

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/SummonerEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/SummonerEndpoint.swift
@@ -78,4 +78,58 @@ class SummonerEndpoint: NSObject {
             })
         }
     }
+    
+    func getMasteriesForSummonerIds(summonerIds: [CLong], completion: (summonerMap: [String: MasteryPagesDto]) -> Void, errorBlock: () -> Void) {
+        Endpoints().summoner_masteriesById(summonerIds: NSArray(array: summonerIds).value(forKey: "description").componentsJoined(by: ",")) { (composedUrl) in
+            AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
+                autoreleasepool({ ()
+                    var newResponse = [String: MasteryPagesDto]()
+                    let json = responseObject as! NSDictionary
+                    let jsonValues = json.allValues as! [[String: AnyObject]]
+                    for i in 0 ..< jsonValues.count {
+                        autoreleasepool({ ()
+                            let oldMasteryPages = jsonValues[i]
+                            
+                            let newMasteryPages = MasteryPagesDto()
+                            autoreleasepool({ ()
+                                let oldPages = oldMasteryPages["pages"] as! [[String: AnyObject]]
+                                
+                                for oldPage in oldPages {
+                                    autoreleasepool({ ()
+                                        let newPage = MasteryPageDto()
+                                        
+                                        newPage.current = oldPage["current"] as! Bool
+                                        newPage.masteryPageId = oldPage["id"] as! CLong
+                                        autoreleasepool({ ()
+                                            let oldMasteries = oldPage[""] as! [[String: AnyObject]]
+                                            for oldMastery in oldMasteries {
+                                                autoreleasepool({ ()
+                                                    let newMastery = MasteryDto()
+                                                    
+                                                    newMastery.masteryId = oldMastery["id"] as! Int
+                                                    newMastery.rank = oldMastery["rank"] as! Int
+                                                    
+                                                    newPage.masteries.append(newMastery)
+                                                })
+                                            }
+                                        })
+                                        newPage.name = oldPage["name"] as! String
+                                        
+                                        newMasteryPages.pages.append(newPage)
+                                    })
+                                }
+                            })
+                            newMasteryPages.summonerId = oldMasteryPages[""] as! CLong
+                            
+                            newResponse[json.allKeys[i] as! String] = newMasteryPages
+                        })
+                    }
+                    completion(summonerMap: newResponse)
+                })
+            }, failure: { (task, error) in
+                errorBlock()
+                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": error.userInfo[AFNetworkingOperationFailingURLResponseErrorKey]!.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+            })
+        }
+    }
 }
