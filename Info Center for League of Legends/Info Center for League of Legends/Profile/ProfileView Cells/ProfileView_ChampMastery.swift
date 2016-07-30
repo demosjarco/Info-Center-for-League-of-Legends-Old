ee18c92293de79c0e24b3833a004b5a0a7772569
Victor Ilisei <vilisei@gmail.com>
2016-07-30 12:01:01 -0700

    Added items that will be in cell

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView_ChampMastery.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView_ChampMastery.swift
@@ -7,7 +7,27 @@
 //
 
 import UIKit
+import MBCircularProgressBar
 
 class ProfileView_ChampMastery: ProfileView_SummaryTile {
+    @IBOutlet var champ1bg:UIImageView?
+    @IBOutlet var champ1squareIcon:UIImageView?
+    @IBOutlet var champ1progressBar:MBCircularProgressBarView?
+    @IBOutlet var champ1masteryIcon:UIImageView?
+    @IBOutlet var champ1masteryScore:UILabel?
+    @IBOutlet var champ1name:UILabel?
     
+    @IBOutlet var champ2bg:UIImageView?
+    @IBOutlet var champ2squareIcon:UIImageView?
+    @IBOutlet var champ2progressBar:MBCircularProgressBarView?
+    @IBOutlet var champ2masteryIcon:UIImageView?
+    @IBOutlet var champ2masteryScore:UILabel?
+    @IBOutlet var champ2name:UILabel?
+    
+    @IBOutlet var champ3bg:UIImageView?
+    @IBOutlet var champ3squareIcon:UIImageView?
+    @IBOutlet var champ3progressBar:MBCircularProgressBarView?
+    @IBOutlet var champ3masteryIcon:UIImageView?
+    @IBOutlet var champ3masteryScore:UILabel?
+    @IBOutlet var champ3name:UILabel?
 }
