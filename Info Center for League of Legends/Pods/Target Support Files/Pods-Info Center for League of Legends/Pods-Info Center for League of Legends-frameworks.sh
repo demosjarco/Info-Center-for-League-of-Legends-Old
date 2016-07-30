e2451b222921f8ce4a34ccf10277d312cd16520a
Victor Ilisei <vilisei@gmail.com>
2016-07-30 08:42:31 -0700

    Added MBCircularProgressBar

--- Info Center for League of Legends/Pods/Target Support Files/Pods-Info Center for League of Legends/Pods-Info Center for League of Legends-frameworks.sh
+++ Info Center for League of Legends/Pods/Target Support Files/Pods-Info Center for League of Legends/Pods-Info Center for League of Legends-frameworks.sh
@@ -86,10 +86,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BEMSimpleLineGraph/BEMSimpleLineGraph.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MBCircularProgressBar/MBCircularProgressBar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SGSStaggeredFlowLayout/SGSStaggeredFlowLayout.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BEMSimpleLineGraph/BEMSimpleLineGraph.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/MBCircularProgressBar/MBCircularProgressBar.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SGSStaggeredFlowLayout/SGSStaggeredFlowLayout.framework"
 fi
