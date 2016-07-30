593ce1acc75195b310a18fc74af9181313114e35
Victor Ilisei <vilisei@gmail.com>
2016-07-30 11:37:57 -0700

    Convert to swift's Float before fabsf then back to CGFloat

--- Info Center for League of Legends/Info Center for League of Legends/Subclasses/StretchyHeaderCollectionViewLayout.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Subclasses/StretchyHeaderCollectionViewLayout.swift
@@ -17,8 +17,8 @@ class StretchyHeaderCollectionViewLayout: UICollectionViewFlowLayout {
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         let collectionView = self.collectionView
         let insets = collectionView!.contentInset
-        var offset = collectionView!.contentOffset
-        var minY = insets.top * -1
+        let offset = collectionView!.contentOffset
+        let minY = CGFloat(fabsf(Float(insets.top)) * -1)
         
         // First get the superclass attributes.
         let attributes = super.layoutAttributesForElements(in: rect)
@@ -26,7 +26,7 @@ class StretchyHeaderCollectionViewLayout: UICollectionViewFlowLayout {
         // Check if we've pulled below past the lowest position
         if offset.y < minY {
             // Figure out how much we've pulled down
-            var deltaY:Float = fabsf(Float(offset.y) - Float(minY))
+            let deltaY = CGFloat(fabsf(Float(offset.y - minY)))
             
             for attrs in attributes! {
                 // Locate the header attributes
@@ -35,8 +35,8 @@ class StretchyHeaderCollectionViewLayout: UICollectionViewFlowLayout {
                     // Adjust the header's height and y based on how much the user has pulled down.
                     let headerSize = self.headerReferenceSize
                     var headerRect = attrs.frame
-                    headerRect.size.height = max(minY, headerSize.height + CGFloat(deltaY))
-                    headerRect.origin.y -= CGFloat(deltaY)
+                    headerRect.size.height = max(minY, headerSize.height + deltaY)
+                    headerRect.origin.y -= deltaY
                     attrs.frame = headerRect
                     break
                 }
