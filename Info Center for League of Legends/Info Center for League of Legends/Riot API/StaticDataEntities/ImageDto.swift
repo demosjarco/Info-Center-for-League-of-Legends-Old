0d317e8cf5ac498dbf4d070aec8643ec5231726b
Victor Ilisei <vilisei@gmail.com>
2016-07-30 16:52:14 -0700

    Created ImageDto

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEntities/ImageDto.swift
@@ -0,0 +1,19 @@
+//
+//  ImageDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 7/30/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ImageDto: NSObject {
+    var full:String = ""
+    var group:String = ""
+    var h:Int = 0
+    var sprite:String = ""
+    var w:Int = 0
+    var x:Int = 0
+    var y:Int = 0
+}
