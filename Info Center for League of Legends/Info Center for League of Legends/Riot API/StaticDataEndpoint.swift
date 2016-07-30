cb5c756923844f63dccf4aef9da6380fef6b9077
Victor Ilisei <vilisei@gmail.com>
2016-07-30 16:34:48 -0700

    Retrieve languages that riot supports from static data

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEndpoint.swift
@@ -12,4 +12,16 @@ import AFNetworking
 
 class StaticDataEndpoint: NSObject {
     
+    func getRegionValidLocales(completion: (languages: [String]) -> Void) {
+        Endpoints().staticData_languages { (composedUrl) in
+            AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
+                autoreleasepool({ ()
+                    let json = responseObject as! [String]
+                    completion(languages: json)
+                })
+            }, failure: { (task, error) in
+                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": error.userInfo[AFNetworkingOperationFailingURLResponseErrorKey]!.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+            })
+        }
+    }
 }
