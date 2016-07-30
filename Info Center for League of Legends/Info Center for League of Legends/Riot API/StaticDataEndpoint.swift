86bf3b3c74f460c866e4171c04173bcda46165cd
Victor Ilisei <vilisei@gmail.com>
2016-07-30 16:49:30 -0700

    Started champion info endpoint

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEndpoint.swift
@@ -11,6 +11,35 @@ import Firebase
 import AFNetworking
 
 class StaticDataEndpoint: NSObject {
+    enum champData:String {
+        case All = "all"
+        case AllyTips = "allytips"
+        case AltImages = "altimages"
+        case Blurb = "blurb"
+        case EnemyTips = "enemytips"
+        case Image = "image"
+        case Info = "info"
+        case Lore = "lore"
+        case Partype = "partype"
+        case Passive = "passive"
+        case RecommendedItems = "recommended"
+        case Skins = "skins"
+        case Spells = "spells"
+        case Stats = "stats"
+        case Tags = "tags"
+    }
+    
+    func getChampionInfoById(champId: Int, championData: champData, completion: () -> Void, notFound: () -> Void, error: () -> Void) {
+        Endpoints().staticData_champion_id(championId: String(champId), champData: championData.rawValue) { (composedUrl) in
+            AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
+                autoreleasepool({ ()
+                    let json = responseObject as! [String: AnyObject]
+                })
+            }, failure: { (task, error) in
+                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": error.userInfo[AFNetworkingOperationFailingURLResponseErrorKey]!.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+            })
+        }
+    }
     
     func getRegionValidLocales(completion: (languages: [String]) -> Void) {
         Endpoints().staticData_languages { (composedUrl) in
