b439bea47a89354783c5eb11da47f0e71b2978ce
Victor Ilisei <vilisei@gmail.com>
2016-08-05 10:10:32 -0700

    Setup server status

---
+++ Info Center for League of Legends/Info Center for League of Legends/News/ServerStatus.swift
@@ -0,0 +1,58 @@
+//
+//  ServerStatus.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 8/5/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ServerStatus: DarkTableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refresh()
+    }
+    
+    @IBAction func closeView() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func refresh() {
+        self.refreshControl?.beginRefreshing()
+        //...
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "<#string#>"
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "incidentCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+}
