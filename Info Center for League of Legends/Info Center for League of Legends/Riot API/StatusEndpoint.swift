890b97689279a1453005c1941d681f4860aeee55
Victor Ilisei <vilisei@gmail.com>
2016-08-05 11:44:11 -0700

    Finished get all shards

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/StatusEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StatusEndpoint.swift
@@ -11,15 +11,41 @@ import Firebase
 import AFNetworking
 
 class StatusEndpoint: NSObject {
-    func getAllShards(completion: (shards: [Shard]) -> Void, error: () -> Void) {
+    func getAllShards(completion: (shards: [Shard]) -> Void, errorBlock: () -> Void) {
         Endpoints().status_shards { (composedUrl) in
-            AFHTTPSessionManager()
+            AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
+                let json = responseObject as! [[String: AnyObject]]
+                var shardArray = [Shard]()
+                for oldShard in json {
+                    let newShard = Shard()
+                    
+                    newShard.hostname = oldShard["hostname"] as! String
+                    newShard.locales = oldShard["locales"] as! [String]
+                    newShard.name = oldShard["name"] as! String
+                    newShard.region_tag = oldShard["region_tag"] as! String
+                    newShard.slug = oldShard["slug"] as! String
+                    
+                    shardArray.append(newShard)
+                }
+                completion(shards: shardArray)
+            }, failure: { (task, error) in
+                let response = task!.response as! HTTPURLResponse
+                errorBlock()
+                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": response.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+            })
         }
     }
     
-    func getShardStatus(completion: (shardStatus: ShardStatus) -> Void, error: () -> Void) {
+    func getShardStatus(completion: (shardStatus: ShardStatus) -> Void, errorBlock: () -> Void) {
         Endpoints().status_byShard { (composedUrl) in
-            AFHTTPSessionManager()
+            AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
+                let json = responseObject as! [String: AnyObject]
+                
+            }, failure: { (task, error) in
+                let response = task!.response as! HTTPURLResponse
+                errorBlock()
+                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": response.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+            })
         }
     }
 }
