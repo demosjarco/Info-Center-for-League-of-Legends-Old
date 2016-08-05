1a301754a2deb4aea37978968c886385200f61dc
Victor Ilisei <vilisei@gmail.com>
2016-08-05 12:58:44 -0700

    Finished status endpoint

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/StatusEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StatusEndpoint.swift
@@ -40,7 +40,74 @@ class StatusEndpoint: NSObject {
         Endpoints().status_byShard { (composedUrl) in
             AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
                 let json = responseObject as! [String: AnyObject]
+                let shardStatus = ShardStatus()
                 
+                shardStatus.hostname = json["hostname"] as! String
+                shardStatus.locales = json["locales"] as! [String]
+                shardStatus.name = json["name"] as! String
+                shardStatus.region_tag = json["region_tag"] as! String
+                let oldServices = json["services"] as! [[String: AnyObject]]
+                for oldService in oldServices {
+                    let newService = Service()
+                    
+                    let oldIncidents = oldService["incidents"] as! [[String: AnyObject]]
+                    for oldIncident in oldIncidents {
+                        let newIncident = Incident()
+                        
+                        newIncident.active = oldIncident["active"] as! Bool
+                        autoreleasepool(invoking: { ()
+                            let dateFormatter = DateFormatter()
+                            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+                            newIncident.created_at = dateFormatter.date(from: oldIncident["created_at"] as! String)!
+                        })
+                        newIncident.incidentId = oldIncident["id"] as! CLong
+                        let oldUpdates = oldIncident["updates"] as! [[String: AnyObject]]
+                        for oldMessage in oldUpdates {
+                            let newMessage = Message()
+                            
+                            newMessage.author = oldMessage["author"] as! String
+                            newMessage.content = oldMessage["content"] as! String
+                            autoreleasepool(invoking: { ()
+                                let dateFormatter = DateFormatter()
+                                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+                                newMessage.created_at = dateFormatter.date(from: oldMessage["created_at"] as! String)!
+                            })
+                            newMessage.messageId = oldMessage["id"] as! String
+                            newMessage.severity = oldMessage["severity"] as! String
+                            let oldTranslations = oldMessage["translations"] as! [[String: AnyObject]]
+                            for oldTranslation in oldTranslations {
+                                let newTranslation = Translation()
+                                
+                                newTranslation.content = oldTranslation["content"] as! String
+                                newTranslation.locale = oldTranslation["locale"] as! String
+                                autoreleasepool(invoking: { ()
+                                    let dateFormatter = DateFormatter()
+                                    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+                                    newTranslation.updated_at = dateFormatter.date(from: oldTranslation["updated_at"] as! String)!
+                                })
+                                
+                                newMessage.translations.append(newTranslation)
+                            }
+                            autoreleasepool(invoking: { ()
+                                let dateFormatter = DateFormatter()
+                                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+                                newMessage.updated_at = dateFormatter.date(from: oldMessage["updated_at"] as! String)!
+                            })
+                            
+                            newIncident.updates.append(newMessage)
+                        }
+                        
+                        newService.incidents.append(newIncident)
+                    }
+                    newService.name = oldService["name"] as! String
+                    newService.slug = oldService["slug"] as! String
+                    newService.status = oldService["status"] as! String
+                    
+                    shardStatus.services.append(newService)
+                }
+                shardStatus.slug = json["slug"] as! String
+                
+                completion(shardStatus: shardStatus)
             }, failure: { (task, error) in
                 let response = task!.response as! HTTPURLResponse
                 errorBlock()
