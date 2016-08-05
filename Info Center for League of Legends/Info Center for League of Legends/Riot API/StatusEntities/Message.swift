744215d899d051bac24e0bdb10ff1540cae37c21
Victor Ilisei <vilisei@gmail.com>
2016-08-05 10:18:08 -0700

    Setup for Status Endpoint

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StatusEntities/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 8/5/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class Message: NSObject {
+    
+}
---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StatusEntities/Shard.swift
@@ -0,0 +1,17 @@
+//
+//  Shard.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 8/5/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class Shard: NSObject {
+    var hostname: String
+    var locales = [String]()
+    var name:String
+    var region_tag:String
+    var slug:String
+}
