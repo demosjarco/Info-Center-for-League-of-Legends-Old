2517ae7325e33de2c2c755892066f6bc13d3172e
Victor Ilisei <vilisei@gmail.com>
2016-08-05 18:11:29 -0700

    Added refreshing status

--- Info Center for League of Legends/Info Center for League of Legends/News/StatusDetail.swift
+++ Info Center for League of Legends/Info Center for League of Legends/News/StatusDetail.swift
@@ -19,7 +19,7 @@ class StatusDetail: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //refreshTimeText = self.toolbarItems![1]
+        refreshTimeText = self.toolbarItems![1]
         
         refresh()
     }
