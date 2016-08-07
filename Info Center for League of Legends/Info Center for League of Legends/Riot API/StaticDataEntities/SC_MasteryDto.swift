0a846fa71404cac13f2adcd4bc51b5259cf8bfa1
Victor Ilisei <vilisei@gmail.com>
2016-08-06 21:08:54 -0700

    Set optional

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEntities/SC_MasteryDto.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEntities/SC_MasteryDto.swift
@@ -11,7 +11,11 @@ import UIKit
 class SC_MasteryDto: NSObject {
     var masteryDescription = [String]()
     var masteryId:Int = 0
-    var image = ImageDto()
+    var image:ImageDto?
     /// Legal values: Cunning, Ferocity, Resolve
-    var masteryTree:String = ""
+    var masteryTree:String?
+    var name:String = ""
+    var prereq:String?
+    var ranks:Int?
+    var sanitizedDescription:String?
 }
