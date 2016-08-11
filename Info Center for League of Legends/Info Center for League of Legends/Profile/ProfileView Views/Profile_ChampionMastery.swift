57430f1a2762edf3b26772d9aa521d13c1ec26c5
Victor Ilisei <vilisei@gmail.com>
2016-08-11 13:50:20 -0700

    prototype champion mastery detail

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_ChampionMastery.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_ChampionMastery.swift
@@ -11,11 +11,17 @@ import UIKit
 class Profile_ChampionMastery: MainCollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*ChampionMasteryEndpoint().getAllChampsBySummonerId(playerId: <#T##CLong#>, completion: { (champions) in
+            // Stuff
+        }, notFound: {
+            // ???
+        }, errorBlock: {
+            // Error
+        })*/
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func closeView() {
+        self.dismiss(animated: true, completion: nil)
     }
 
     /*
@@ -31,14 +37,13 @@ class Profile_ChampionMastery: MainCollectionViewController {
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 132
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -48,36 +53,4 @@ class Profile_ChampionMastery: MainCollectionViewController {
     
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: AnyObject?) {
-    
-    }
-    */
-
 }
