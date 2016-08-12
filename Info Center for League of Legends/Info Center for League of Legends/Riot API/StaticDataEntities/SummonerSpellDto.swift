5b24578d878c1dded18a1464982ac05433c36746
Victor Ilisei <vilisei@gmail.com>
2016-08-12 14:22:42 -0700

    Created entities for summoner spell

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEntities/SummonerSpellDto.swift
@@ -0,0 +1,13 @@
+//
+//  SummonerSpellDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 8/12/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class SummonerSpellDto: NSObject {
+    
+}
