18034e1fea6a5e60f189c4013fca3bbaf5ca882c
Victor Ilisei <vilisei@gmail.com>
2016-08-12 14:32:21 -0700

    Created properties

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEntities/SummonerSpellDto.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/StaticDataEntities/SummonerSpellDto.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class SummonerSpellDto: NSObject {
-    
+    var cooldown:[Double]?
+    var cooldownBurn:String?
+    var cost:[Int]?
+    var costBurn:String?
+    var costType:String?
+    var spellDescription:String = ""
+    /// This field is a List of List of Double.
+    var effect:[[Double]]?
+    var effectBurn:[String]?
+    var spellId:Int = 0
+    var image:ImageDto?
+    var key:String = ""
+    var leveltip:LevelTipDto?
+    var maxrank:Int?
+    var modes:[String]?
+    var name:String = ""
+    /// If 0, then spell targets one's own champion.
+    var range:[Int]?
+    var rangeBurn:String?
+    var resource:String?
+    var sanitizedDescription:String?
+    var sanitizedTooltip:String?
+    var summonerLevel:Int = 0
+    var tooltip:String?
+    var vars:[SpellVarsDto]?
 }
