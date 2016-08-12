ea558c937d4bf674486c2cfb53948f0603a857ad
Victor Ilisei <vilisei@gmail.com>
2016-08-12 12:19:05 -0700

    Added properties

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_RecentGames_Cell.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_RecentGames_Cell.swift
@@ -9,8 +9,22 @@
 import UIKit
 
 class Profile_RecentGames_Cell: UITableViewCell {
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    @IBOutlet var champIcon:UIImageView?
+    @IBOutlet var champIconCutout:UIView?
+    @IBOutlet var gameOutcome:UILabel?
+    @IBOutlet var gameType:UILabel?
+    @IBOutlet var summonerSpell1:UIImageView?
+    @IBOutlet var summonerSpell2:UIImageView?
+    @IBOutlet var item1:UIImageView?
+    @IBOutlet var item2:UIImageView?
+    @IBOutlet var item3:UIImageView?
+    @IBOutlet var item4:UIImageView?
+    @IBOutlet var item5:UIImageView?
+    @IBOutlet var item6:UIImageView?
+    @IBOutlet var item7:UIImageView?
+    @IBOutlet var kda:UILabel?
+    @IBOutlet var creepScore:UILabel?
+    @IBOutlet var gold:UILabel?
+//    @IBOutlet var <#name#>:<#type#>?
+    
 }
