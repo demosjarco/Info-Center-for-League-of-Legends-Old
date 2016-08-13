8782dd79cc8aa55caf632d83895663fc2a591b5b
Victor Ilisei <vilisei@gmail.com>
2016-08-12 21:20:42 -0700

    Added more properties

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/GameEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/GameEndpoint.swift
@@ -38,40 +38,40 @@ class GameEndpoint: NSObject {
                     let oldStats = oldGame["stats"] as! [String: AnyObject]
                     
                     newStats.assists = oldStats["assists"] as? Int
-                    
-                    
+                    newStats.barracksKilled = oldStats["barracksKilled"] as? Int
+                    newStats.bountyLevel = oldStats["bountyLevel"] as? Int
                     newStats.championsKilled = oldStats["championsKilled"] as? Int
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                    newStats.combatPlayerScore = oldStats["combatPlayerScore"] as? Int
+                    newStats.consumablesPurchased = oldStats["consumablesPurchased"] as? Int
+                    newStats.damageDealtPlayer = oldStats["damageDealtPlayer"] as? Int
+                    newStats.doubleKills = oldStats["doubleKills"] as? Int
+                    newStats.firstBlood = oldStats["firstBlood"] as? Int
+                    newStats.gold = oldStats["gold"] as? Int
+                    newStats.goldEarned = oldStats["goldEarned"] as? Int
+                    newStats.goldSpent = oldStats["goldSpent"] as? Int
+                    newStats.item0 = oldStats["item0"] as? Int
+                    newStats.item1 = oldStats["item1"] as? Int
+                    newStats.item2 = oldStats["item2"] as? Int
+                    newStats.item3 = oldStats["item3"] as? Int
+                    newStats.item4 = oldStats["item4"] as? Int
+                    newStats.item5 = oldStats["item5"] as? Int
+                    newStats.item6 = oldStats["item6"] as? Int
+                    newStats.itemsPurchased = oldStats["itemsPurchased"] as? Int
+                    newStats.killingSprees = oldStats["killingSprees"] as? Int
+                    newStats.largestCriticalStrike = oldStats["largestCriticalStrike"] as? Int
+                    newStats.largestKillingSpree = oldStats["largestKillingSpree"] as? Int
+                    newStats.largestMultiKill = oldStats["largestMultiKill"] as? Int
+                    newStats.legendaryItemsCreated = oldStats["legendaryItemsCreated"] as? Int
                     newStats.level = oldStats["level"] as? Int
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                    newStats.magicDamageDealtPlayer = oldStats["magicDamageDealtPlayer"] as? Int
+                    newStats.magicDamageDealtToChampions = oldStats["magicDamageDealtToChampions"] as? Int
+                    newStats.magicDamageTaken = oldStats["magicDamageTaken"] as? Int
+                    newStats.minionsDenied = oldStats["minionsDenied"] as? Int
+                    newStats.minionsKilled = oldStats["minionsKilled"] as? Int
+                    newStats.neutralMinionsKilled = oldStats["neutralMinionsKilled"] as? Int
+                    newStats.neutralMinionsKilledEnemyJungle = oldStats["neutralMinionsKilledEnemyJungle"] as? Int
+                    newStats.neutralMinionsKilledYourJungle = oldStats["neutralMinionsKilledYourJungle"] as? Int
+                    // newStats.<#property#> = oldStats["<#key#>"] as? Int
                     
                     
                     
