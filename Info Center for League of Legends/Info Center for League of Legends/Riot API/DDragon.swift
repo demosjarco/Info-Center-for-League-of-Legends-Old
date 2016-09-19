7d2460fb159bc493d98546c3795b302b4c838272
Victor Ilisei <vilisei@gmail.com>
2016-09-19 08:33:26 -0700

    Removed parameter name from callback

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/DDragon.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/DDragon.swift
@@ -21,14 +21,14 @@ class DDragon: NSObject {
     func getCDNurl(completion: @escaping (_ cdnUrl: String) -> Void) {
         AFHTTPSessionManager().get("http://ddragon.leagueoflegends.com/realms/" + Endpoints().getRegion() + ".json", parameters: nil, progress: nil, success: { (task, responseObject) in
             let dict = responseObject as! NSDictionary
-            completion(cdnUrl: dict["cdn"] as! String)
+            completion(dict["cdn"] as! String)
         }, failure: nil)
     }
     
     func getLatestDDragonVersion(dataType: String, completion: @escaping (_ version: String) -> Void) {
         AFHTTPSessionManager().get("http://ddragon.leagueoflegends.com/realms/" + Endpoints().getRegion() + ".json", parameters: nil, progress: nil, success: { (task, responseObject) in
             let dict = responseObject as! NSDictionary
-            completion(version: dict["n"]![dataType] as! String)
+            completion(dict["n"]![dataType] as! String)
         }, failure: nil)
     }
     
