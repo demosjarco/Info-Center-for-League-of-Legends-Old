706e5f93014d8f02da84ca8a2c85ed3e4b30d503
Victor Ilisei <vilisei@gmail.com>
2016-10-07 12:56:18 -0700

    Created custom tournament list cell

---
+++ Info Center for League of Legends/Info Center for League of Legends/Tournaments/TournamentListCell.swift
@@ -0,0 +1,22 @@
+//
+//  TournamentListCell.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 10/7/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class TournamentListCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
