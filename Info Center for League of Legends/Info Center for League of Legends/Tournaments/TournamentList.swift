0247a562b6b067cf88e611de34df9db843f9faf8
Victor Ilisei <vilisei@gmail.com>
2016-10-13 16:52:11 -0700

    Removed refresh method since it will be realtime

--- Info Center for League of Legends/Info Center for League of Legends/Tournaments/TournamentList.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Tournaments/TournamentList.swift
@@ -20,12 +20,6 @@ class TournamentList: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        refresh()
-    }
-    
-    @IBAction func refresh() {
-        self.refreshControl?.beginRefreshing()
-        //FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": response.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
     }
 
     // MARK: - Table view data source
