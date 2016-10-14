b2ddb54509cf227ab315f55a4bf9e628ad262402
Victor Ilisei <vilisei@gmail.com>
2016-10-14 08:26:18 -0700

    Only edit buttons if second section

--- Info Center for League of Legends/Info Center for League of Legends/Tournaments/AccountSettings/AccountSettings.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Tournaments/AccountSettings/AccountSettings.swift
@@ -392,7 +392,11 @@ class AccountSettings: UITableViewController, UITextFieldDelegate, UIPopoverPres
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        if indexPath.section == 0 {
+            return false
+        } else {
+            return true
+        }
     }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
