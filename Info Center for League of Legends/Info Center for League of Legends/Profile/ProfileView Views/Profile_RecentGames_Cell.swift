539060cf292cc27a60bfcaf66d6b641f62119ea2
Victor Ilisei <vilisei@gmail.com>
2016-12-21 14:20:59 -0700

    Renamed to item 0-6 instead of 1-7

--- Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_RecentGames_Cell.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_RecentGames_Cell.swift
@@ -15,13 +15,13 @@ class Profile_RecentGames_Cell: UITableViewCell {
     @IBOutlet var gameType:UILabel?
     @IBOutlet var summonerSpell1:UIImageView?
     @IBOutlet var summonerSpell2:UIImageView?
+    @IBOutlet var item0:UIImageView?
     @IBOutlet var item1:UIImageView?
     @IBOutlet var item2:UIImageView?
     @IBOutlet var item3:UIImageView?
     @IBOutlet var item4:UIImageView?
     @IBOutlet var item5:UIImageView?
     @IBOutlet var item6:UIImageView?
-    @IBOutlet var item7:UIImageView?
     @IBOutlet var kda:UILabel?
     @IBOutlet var creepScore:UILabel?
     @IBOutlet var creepScoreIcon:UIImageView?
