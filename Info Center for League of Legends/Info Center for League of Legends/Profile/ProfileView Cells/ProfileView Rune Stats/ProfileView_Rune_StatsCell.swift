d7895187baefb4434a5cf70d55566b764f539188
Victor Ilisei <vilisei@gmail.com>
2016-12-22 10:05:18 -0700

    Created runes stats cell

---
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Cells/ProfileView Rune Stats/ProfileView_Rune_StatsCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileView_Rune_StatsCell.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 12/22/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView_Rune_StatsCell: UICollectionViewCell {
+    @IBOutlet var runeIcon:UIImageView?
+    @IBOutlet var runeStat:UILabel?
+}
