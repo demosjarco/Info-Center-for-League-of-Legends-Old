569b71df0ae02dfed9518b5b0009cb880a3cc6a0
Victor Ilisei <vilisei@gmail.com>
2016-12-30 20:07:37 -0700

    Created current game view

---
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_CurrentGame.swift
@@ -0,0 +1,46 @@
+//
+//  Profile_CurrentGame.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 12/30/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class Profile_CurrentGame: MainTableViewController {
+    var summoner = SummonerDto()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+}
