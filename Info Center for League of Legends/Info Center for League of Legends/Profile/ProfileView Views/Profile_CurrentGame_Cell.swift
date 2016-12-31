57d7674d056f0c0bfbaaee3c932b018f81bf3446
Victor Ilisei <vilisei@gmail.com>
2016-12-30 20:36:11 -0700

    Created custom cell and header for current game view

---
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_CurrentGame_Cell.swift
@@ -0,0 +1,22 @@
+//
+//  Profile_CurrentGame_Cell.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 12/30/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class Profile_CurrentGame_Cell: UITableViewCell {
+    @IBOutlet var champIcon:UIImageView?
+    @IBOutlet var champMastery:UIImageView?
+    @IBOutlet var spell1:UIImageView?
+    @IBOutlet var spell2:UIImageView?
+    @IBOutlet var keyStoneMastery:UIImageView?
+    @IBOutlet var summonerName:UILabel?
+    @IBOutlet var currentSeasonRank:UIImageView?
+    @IBOutlet var lastSeasonRank:UIImageView?
+    @IBOutlet var underLevel:UILabel?
+    @IBOutlet var champBan:UIImageView?
+}
---
+++ Info Center for League of Legends/Info Center for League of Legends/Profile/ProfileView Views/Profile_CurrentGame_Header.swift
@@ -0,0 +1,16 @@
+//
+//  Profile_CurrentGame_Header.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 12/30/16.
+//  Copyright © 2016 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class Profile_CurrentGame_Header: UIView {
+    @IBOutlet var teamName:UILabel?
+    @IBOutlet var currentSeason:UILabel?
+    @IBOutlet var lastSeason:UILabel?
+    @IBOutlet var bans:UILabel?
+}
