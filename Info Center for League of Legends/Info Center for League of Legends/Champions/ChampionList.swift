53a1aa489478e1691e8e4d995c24dacedb8e298c
Victor Ilisei <vilisei@gmail.com>
2017-01-02 15:22:22 -0700

    Setup champion list view

--- Info Center for League of Legends/Info Center for League of Legends/Champions/ChampionList.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Champions/ChampionList.swift
@@ -8,20 +8,67 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class ChampionList: MainCollectionViewController {
+    @IBOutlet var refresher:UIRefreshControl?
+    
+    var champions = [[ChampionDto](), [ChampionDto]()]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
+        refresher = UIRefreshControl(frame: self.collectionView!.frame)
+        refresher?.tintColor = UIColor.lightText
+        refresher?.addTarget(self, action: #selector(self.refresh), for: .valueChanged)
+        self.collectionView?.insertSubview(refresher!, at: self.collectionView!.subviews.count - 1)
+        
+        refresh()
+    }
+    
+    @IBAction func refresh() {
+        refresher?.beginRefreshing()
+        self.champions = [[ChampionDto](), [ChampionDto]()]
+        self.collectionView?.reloadData()
+        
+        ChampionEndpoint().getAllChampions(false, completion: { (championList) in
+            for champion in championList.champions {
+                autoreleasepool(invoking: { ()
+                    StaticDataEndpoint().getChampionInfoById(Int(champion.champId), championData: StaticDataEndpoint.champData.Image, completion: { (champInfo) in
+                        autoreleasepool(invoking: { ()
+                            if champion.freeToPlay {
+                                self.champions[0].append(champInfo)
+                                self.collectionView?.reloadSections(IndexSet(integer: 0))
+                            } else {
+                                self.champions[1].append(champInfo)
+                                self.collectionView?.reloadSections(IndexSet(integer: 1))
+                            }
+                            
+                            self.champions[0].sort(by: { (champ1, champ2) -> Bool in
+                                return champ1.name < champ2.name
+                            })
+                            self.collectionView?.reloadSections(IndexSet(integer: 0))
+                            
+                            self.champions[1].sort(by: { (champ1, champ2) -> Bool in
+                                return champ1.name < champ2.name
+                            })
+                            self.collectionView?.reloadSections(IndexSet(integer: 1))
+                            
+                            if self.champions[0].count + self.champions[1].count == championList.champions.count {
+                                self.refresher?.endRefreshing()
+                            }
+                        })
+                    }, notFound: {
+                        // ??
+                        self.refresher?.endRefreshing()
+                    }, errorBlock: {
+                        // Error
+                        self.refresher?.endRefreshing()
+                    })
+                })
+            }
+        }, errorBlock: {
+            // Error
+            self.refresher?.endRefreshing()
+        })
     }
 
     // MARK: - Navigation
@@ -35,19 +82,32 @@ class ChampionList: MainCollectionViewController {
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return self.champions.count
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return self.champions[section].count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "champCell", for: indexPath) as! ChampionListCell
+        // Clear cell
+        cell.champIcon?.image = nil
+        
         // Configure the cell
+        // Use the new LCU icon if exists
+        if let champIcon = DDragon().getLcuChampionSquareArt(champId: self.champions[indexPath.section][indexPath.row].champId) {
+            cell.champIcon?.image = champIcon
+        } else {
+            DDragon().getChampionSquareArt(self.champions[indexPath.section][indexPath.row].image!.full, completion: { (champSquareArtUrl) in
+                cell.champIcon?.setImageWith(champSquareArtUrl)
+            })
+        }
+        
+        cell.freeToPlayBanner?.isHidden = indexPath.section == 1 ? true : false
+        
+        cell.champName?.text = self.champions[indexPath.section][indexPath.row].name
     
         return cell
     }
