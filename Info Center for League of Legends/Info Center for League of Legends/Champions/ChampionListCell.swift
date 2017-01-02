775478bb81bb497cdc80b6f6c11618efc1b795f3
Victor Ilisei <vilisei@gmail.com>
2017-01-02 14:41:31 -0700

    Created champion list custom cell

---
+++ Info Center for League of Legends/Info Center for League of Legends/Champions/ChampionListCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChampionListCell.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 1/2/17.
+//  Copyright © 2017 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ChampionListCell: UICollectionViewCell {
+    @IBOutlet var freeToPlayBanner:UIImageView?
+    @IBOutlet var champIcon:UIImageView?
+    @IBOutlet var champName:UILabel?
+}
