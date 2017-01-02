58afee51b052415f2787a3e1a488f8219418fc74
Victor Ilisei <vilisei@gmail.com>
2017-01-02 13:58:25 -0700

    Created /champions endpoint entities

---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/ChampionEndpointEntities/ChampionInfoDto.swift
@@ -0,0 +1,19 @@
+//
+//  ChampionInfoDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 1/2/17.
+//  Copyright © 2017 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ChampionInfoDto: NSObject {
+    var active = false
+    var botEnabled = false
+    var botMmEnabled = false
+    var freeToPlay = false
+    var champId:CLong = 0
+    var rankedPlayEnabled = false
+    
+}
---
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/ChampionEndpointEntities/ChampionListDto.swift
@@ -0,0 +1,13 @@
+//
+//  ChampionListDto.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 1/2/17.
+//  Copyright © 2017 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ChampionListDto: NSObject {
+    var champions = [ChampionInfoDto]()
+}
