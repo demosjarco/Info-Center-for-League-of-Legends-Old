130b4b38de16c5eba8aab18abb4ffc3a92501c90
Victor Ilisei <vilisei@gmail.com>
2017-01-05 00:30:22 -0700

    Created header view

---
+++ Info Center for League of Legends/Info Center for League of Legends/Champions/ChampionDetail Views/ChampionDetail_Content_Header.swift
@@ -0,0 +1,17 @@
+//
+//  ChampionDetail_Content_Header.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 1/5/17.
+//  Copyright © 2017 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+protocol ChampViewHeaderDelegate {
+    func goBack()
+}
+
+class ChampionDetail_Content_Header: UICollectionReusableView {
+    var delegate:ChampViewHeaderDelegate?
+}
