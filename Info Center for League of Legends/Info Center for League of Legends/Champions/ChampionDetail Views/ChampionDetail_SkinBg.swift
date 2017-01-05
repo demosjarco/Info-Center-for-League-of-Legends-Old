da6c2a73734a753f38f78db23d3925e112b6cadb
Victor Ilisei <vilisei@gmail.com>
2017-01-04 22:22:01 -0700

    Added skin background view for champion detail

---
+++ Info Center for League of Legends/Info Center for League of Legends/Champions/ChampionDetail Views/ChampionDetail_SkinBg.swift
@@ -0,0 +1,31 @@
+//
+//  ChampionDetail_SkinBg.swift
+//  Info Center for League of Legends
+//
+//  Created by Victor Ilisei on 1/4/17.
+//  Copyright © 2017 Tech Genius. All rights reserved.
+//
+
+import UIKit
+
+class ChampionDetail_SkinBg: UIViewController {
+    @IBOutlet var skinImage:UIImageView?
+    
+    var pageIndex = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
