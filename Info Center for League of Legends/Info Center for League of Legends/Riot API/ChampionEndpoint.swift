1ca1b73aa70560e46da0c3c148707f82fd63a492
Victor Ilisei <vilisei@gmail.com>
2017-02-03 16:28:44 -0700

    Moved over to raw NSURLSession instead of AFNetworking and save to core data on each successful HTTP load

--- Info Center for League of Legends/Info Center for League of Legends/Riot API/ChampionEndpoint.swift
+++ Info Center for League of Legends/Info Center for League of Legends/Riot API/ChampionEndpoint.swift
@@ -8,38 +8,78 @@
 
 import Foundation
 import Firebase
-import AFNetworking
+import CoreData
 
 class ChampionEndpoint: NSObject {
     func getAllChampions(_ freeToPlay: Bool, completion: @escaping (_ championList: ChampionListDto) -> Void, errorBlock: @escaping () -> Void) {
         Endpoints().champions(freeToPlay ? "true" : "false") { (composedUrl) in
-            AFHTTPSessionManager().get(composedUrl, parameters: nil, progress: nil, success: { (task, responseObject) in
-                autoreleasepool(invoking: { ()
-                    let json = responseObject as! [String: [[String: AnyObject]]]
-                    let newChampionList = ChampionListDto()
-                    
-                    let oldChampions = json["champions"]!
-                    for oldChampion in oldChampions {
-                        autoreleasepool { ()
-                            let newChampion = ChampionInfoDto()
-                            
-                            newChampion.active = oldChampion["active"] as! Bool
-                            newChampion.botEnabled = oldChampion["botEnabled"] as! Bool
-                            newChampion.botMmEnabled = oldChampion["botMmEnabled"] as! Bool
-                            newChampion.freeToPlay = oldChampion["freeToPlay"] as! Bool
-                            newChampion.champId = oldChampion["id"] as! CLong
-                            newChampion.rankedPlayEnabled = oldChampion["rankedPlayEnabled"] as! Bool
-                            
-                            newChampionList.champions.append(newChampion)
-                        }
+            autoreleasepool(invoking: { ()
+                DispatchQueue.main.async { [unowned self] in
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+                }
+                
+                URLSession(configuration: URLSessionConfiguration.default).dataTask(with: URL(string: composedUrl)!, completionHandler: { (data, response, error) in
+                    DispatchQueue.main.async { [unowned self] in
+                        UIApplication.shared.isNetworkActivityIndicatorVisible = true
                     }
                     
-                    completion(newChampionList)
-                })
-            }, failure: { (task, error) in
-                let response = task!.response as! HTTPURLResponse
-                errorBlock()
-                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": NSDate().timeIntervalSince1970, "httpCode": response.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+                    DispatchQueue.global(qos: DispatchQoS.QoSClass.userInitiated).async { [unowned self] in
+                        if let error = error {
+                            print(error.localizedDescription)
+                        } else if let httpResponse = response as? HTTPURLResponse {
+                            if httpResponse.statusCode == 200 {
+                                if let data = data {
+                                    do {
+                                        if let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0)) as? [String: [[String: AnyObject]]] {
+                                            let newChampionList = ChampionListDto()
+                                            
+                                            let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+                                            let c_list_entity = NSEntityDescription.entity(forEntityName: "C_1_2_List", in: managedContext)
+                                            let c_list = NSManagedObject(entity: c_list_entity!, insertInto: managedContext)
+                                            
+                                            let oldChampions = json["champions"]!
+                                            for oldChampion in oldChampions {
+                                                autoreleasepool { ()
+                                                    let newChampion = ChampionInfoDto()
+                                                    
+                                                    let c_info_entity = NSEntityDescription.entity(forEntityName: "C_1_2_Info", in: managedContext)
+                                                    let c_info = NSManagedObject(entity: c_info_entity!, insertInto: managedContext)
+                                                    
+                                                    newChampion.active = oldChampion["active"] as! Bool
+                                                    c_info.setValue(oldChampion["active"] as! Bool, forKey: "active")
+                                                    newChampion.botEnabled = oldChampion["botEnabled"] as! Bool
+                                                    c_info.setValue(oldChampion["botEnabled"] as! Bool, forKey: "botEnabled")
+                                                    newChampion.botMmEnabled = oldChampion["botMmEnabled"] as! Bool
+                                                    c_info.setValue(oldChampion["botMmEnabled"] as! Bool, forKey: "botMmEnabled")
+                                                    newChampion.freeToPlay = oldChampion["freeToPlay"] as! Bool
+                                                    c_info.setValue(oldChampion["freeToPlay"] as! Bool, forKey: "freeToPlay")
+                                                    newChampion.champId = oldChampion["id"] as! CLong
+                                                    c_info.setValue(oldChampion["id"] as! Int32, forKey: "champId")
+                                                    newChampion.rankedPlayEnabled = oldChampion["rankedPlayEnabled"] as! Bool
+                                                    c_info.setValue(oldChampion["rankedPlayEnabled"] as! Bool, forKey: "rankedPlayEnabled")
+                                                    
+                                                    newChampionList.champions.append(newChampion)
+                                                    c_list.setValue(c_info, forKey: "champInfo")
+                                                }
+                                            }
+                                            
+                                            do {
+                                                try managedContext.save()
+                                            } catch let saveError as NSError {
+                                                print("Error saving to core data: \(saveError.localizedDescription)")
+                                            }
+                                            completion(newChampionList)
+                                        }
+                                    } catch let jsonError as NSError {
+                                        print("Error parsing results: \(jsonError.localizedDescription)")
+                                    }
+                                }
+                            } else {
+                                FIRDatabase.database().reference().child("api_error").childByAutoId().updateChildValues(["datestamp": Date().timeIntervalSince1970, "httpCode": httpResponse.statusCode, "url": composedUrl, "deviceModel": Endpoints().getDeviceModel(), "deviceVersion": UIDevice().systemVersion])
+                            }
+                        }
+                    }
+                }).resume()
             })
         }
     }
